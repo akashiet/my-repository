68516b6b1165f04041dca076ca677ea7cf2059d7
akashiet <91930473+akashiet@users.noreply.github.com>
2025-01-08 14:27:11 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+Class Test{
+  public static void main(String[] args){
+    int first=10;
+    int second=20;
+    // add two num
+  int sum=first+second;
+    System.out.println(first+ "+" +second + " = " + sum);
+  }
+}
+    
